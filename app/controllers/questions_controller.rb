183d95f80f7dfe46e7284473b32f736404079415
Walter Jr <wj@tomcix.com>
2020-10-09 12:03:42 -0300

    Exercise # revised with test features

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,17 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    ask = params[:question]
+    @ask = params[:question]
 
-    if ask.present?
-
-      if ask.downcase == 'i am going to work'
-        @answer = 'Great!'
-      elsif ask.downcase.end_with?('?')
-        @answer = 'Silly question, get dressed and go to work!'
-      else
-        @answer = 'I dont care, get dressed and go to work!'
-      end
+    if @ask.blank?
+      @answer = "I can't hear you!"
+    elsif @ask.downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif @ask.downcase.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
